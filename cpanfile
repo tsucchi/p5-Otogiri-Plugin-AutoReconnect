a5e9130f4d079da7cb338d5d13b30b653b345f74
Takuya Tsuchida <tsucchi@cpan.org>
2014-12-30 10:59:31 +0900

    fork support

--- cpanfile
+++ cpanfile
@@ -1,5 +1,5 @@
-requires 'Otogiri::Plugin';
 requires 'perl', '5.008001';
+requires 'Otogiri::Plugin', '0.03';
 
 on configure => sub {
     requires 'Module::Build::Tiny', '0.035';
@@ -7,6 +7,7 @@ on configure => sub {
 
 on test => sub {
     requires 'Otogiri';
+    requires 'File::Temp';
     requires 'DBD::SQLite';
     requires 'Test::More', '0.98';
 };
--- lib/Otogiri/Plugin/AutoReconnect.pm
+++ lib/Otogiri/Plugin/AutoReconnect.pm
@@ -8,7 +8,7 @@ use Carp qw();
 
 our $VERSION = "0.01";
 
-our @EXPORT = qw(reconnect _in_transaction_check);
+our @EXPORT = qw(reconnect owner_pid _in_transaction_check);
 
 BEGIN {
     no warnings 'redefine';
@@ -21,18 +21,38 @@ sub reconnect {
     $self->_in_transaction_check();
 
     my $dbh = $self->{dbh};
-    $dbh->disconnect();
+    $self->{dbh}->disconnect();
+    $self->owner_pid(undef);
 
-    $self->{dbh} = DBIx::Sunny->connect(@{$self->{connect_info}});
+    $self->{dbh} = $dbh->clone();
+    $self->owner_pid($$);
 }
 
 sub dbh {
     my ($self) = @_;
     my $dbh = $self->{dbh};
+
+    if ( !defined $self->owner_pid ) {
+        $self->owner_pid($$);
+    }
+    if ( $self->owner_pid != $$ ) {
+        $self->reconnect;
+    }
     if ( !$dbh->FETCH('Active') || !$dbh->ping ) {
         $self->reconnect;
     }
     return $self->{dbh};
+}
+
+sub owner_pid {
+    my $self = shift;
+
+    if ( @_ ) {
+        $self->{owner_pid} = $_[0];
+    }
+    else {
+        return $self->{owner_pid};
+    }
 }
 
 sub _in_transaction_check {
@@ -68,6 +88,10 @@ Otogiri::Plugin::AutoReconnect - Otogiri plugin automatically reconnect when con
 
 Otogiri::Plugin::AutoReconnect is connection manager for Otogiri. When this plugin is loaded and database connection is 
 lost, automatically reconnect to database.
+
+=head1 SEE ALSO
+
+L<Teng>, L<DBIx::TransactionManager>, L<DBIx::Connector>
 
 =head1 LICENSE
 
